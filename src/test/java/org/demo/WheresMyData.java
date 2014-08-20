0cecc222f7354715ab8427100d90f44b605d65a3
bep <bjorn.erik.pedersen@gmail.com>
2014-08-20 19:30:42 +0200

    Fix table-drop issues.

--- src/test/java/org/demo/WheresMyData.java
+++ src/test/java/org/demo/WheresMyData.java
@@ -7,11 +7,11 @@ import org.demo.repository.CategoryRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=Config.class)
+@SpringApplicationConfiguration(classes=Config.class)
 public class WheresMyData {
 
 	@Autowired CategoryRepository repository;
